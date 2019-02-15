a297ec56ec01cf1b8d00ce3e80882cf9c8f520c0
Rob McDiarmid <rmcdiarmid@konradgroup.com>
2019-02-15 14:02:28 -0500

    added ellipse and node scaling for 2D layers

--- network_visualizer_ui/mixins/layer-renderer-2d.mixin.js
+++ network_visualizer_ui/mixins/layer-renderer-2d.mixin.js
@@ -1,6 +1,8 @@
 (function(){
-    pointSize = 3;
-    pointArea = pointSize**2;
+    let nodeSpacing = 10;
+    let ellipseWidth = 21;
+    let pointSize = 3;
+
 
     function getCanvasByName(nodeContainerElem, name) {
         let canvas = nodeContainerElem.querySelector('#' + name);
@@ -14,29 +16,33 @@
         return canvas;
     }
 
-    function imageBuffersFromChannelArray(channelArray, pixelPositions) {
-        height = channelArray.length;
-        width = channelArray[0].length;
-        numChannels = channelArray[0][0].length;
+    function imageBuffersFromChannelArray(channelArray, pointSize, nodeScaleFactor, pixelPositions) {
+        let pointArea = pointSize**2;
+        let height = channelArray.length;
+        let width = channelArray[0].length;
+        let numChannels = channelArray[0][0].length;
 
-        imgBuffers = [];
+        let imgBuffers = [];
 
         for(let channel = 0; channel < numChannels; channel++) {
-            buffer = new Uint8ClampedArray(width * height * pointArea * 4);
+            let buffer = new Uint8ClampedArray(width * height * pointArea * 4);
 
             //Based on https://stackoverflow.com/questions/22823752/creating-image-from-array-in-javascript-and-html5
             for(let y = 0; y < height; y++) {
                 for(let x = 0; x < width; x++) {
-                    // let pixelIndex = channel + x * numChannels + y * numChannels * width;
-                    // pixelPositions[pixelIndex] = {x: x*pointSize, y: y*pointSize, channel: channel};
-                    pixelPositions.push({x: x*pointSize, y: y*pointSize, channel: channel});
+                    pixelPositions.push({
+                        x: x*pointSize*nodeScaleFactor,
+                        y: y*pointSize*nodeScaleFactor,
+                        channel,
+                        pointSize
+                    });
 
                     for (let innerPointXOffset = 0; innerPointXOffset < pointSize; innerPointXOffset++) {
                         for (let innerPointYOffset = 0; innerPointYOffset < pointSize; innerPointYOffset++) {
-                            pixelRow = y * pointSize + innerPointYOffset;
-                            pixelCol = x * pointSize + innerPointXOffset;
-                            pixelOffset = (pixelRow * width * pointSize + pixelCol) * 4;
-                            greyscaleValue = channelArray[y][x][channel] * 255;
+                            let pixelRow = y * pointSize + innerPointYOffset;
+                            let pixelCol = x * pointSize + innerPointXOffset;
+                            let pixelOffset = (pixelRow * width * pointSize + pixelCol) * 4;
+                            let greyscaleValue = channelArray[y][x][channel] * 255;
 
                             buffer[pixelOffset] = greyscaleValue; // Red
                             buffer[pixelOffset + 1] = greyscaleValue; // Green
@@ -66,19 +72,78 @@
                 this.layerRenderer2DNodeElems = [];
                 this.layerRenderer2DPixelPositions = [];
 
-                let buffers = imageBuffersFromChannelArray(outputs, this.layerRenderer2DPixelPositions);
+                let containerComputedStyle = getComputedStyle(nodeContainerElem);
+                let containerPadding = parseFloat(containerComputedStyle.paddingLeft) + parseFloat(containerComputedStyle.paddingRight);
+                let containerWidth = nodeContainerElem.clientWidth - containerPadding;
 
                 let height = outputs.length;
                 let width = outputs[0].length;
-                let channels = outputs[0][0].length;
+                let totalChannels = outputs[0][0].length;
 
-                let maxChannels = 7;
-                channels = Math.min(channels, maxChannels);
+                let minChannels = Math.min(4, totalChannels);
 
-                for(let channel = 0; channel < channels; channel++) {
+                let showEllipse = minChannels < totalChannels;
+                let effectiveEllipseWidth = showEllipse ? ellipseWidth : 0;
+
+                let nodeRawWidth = width * pointSize;
+                let nodeRawHeight = height * pointSize;
+                let nodeWithSpacingRawWidth = nodeRawWidth + nodeSpacing;
+                let layerRawWidth = nodeWithSpacingRawWidth * minChannels + effectiveEllipseWidth;
+
+                let nodeWidth, nodeHeight, numChannels, nodeScaleFactor;
+
+                //Scale down nodes to fit in container
+                if(layerRawWidth > containerWidth) {
+                    numChannels = minChannels;
+                    nodeWidth = (containerWidth - (nodeSpacing * numChannels) - effectiveEllipseWidth) / numChannels;
+                    nodeScaleFactor = nodeWidth / (width*pointSize);
+                    nodeHeight =  (height*pointSize) * nodeScaleFactor;
+
+                //Add more nodes and scale up to fill container
+                } else {
+                    numChannels = Math.floor((containerWidth - effectiveEllipseWidth) / nodeWithSpacingRawWidth);
+
+                    showEllipse = numChannels < totalChannels;
+                    effectiveEllipseWidth = showEllipse ? ellipseWidth : 0;
+
+                    nodeScaleFactor = (containerWidth - (nodeSpacing * numChannels) - effectiveEllipseWidth) / (numChannels * nodeRawWidth);
+                    nodeScaleFactor = Math.min(1.5, nodeScaleFactor);
+                    nodeWidth = nodeRawWidth * nodeScaleFactor;
+                    nodeHeight = nodeRawHeight * nodeScaleFactor;
+                }
+
+
+                let buffers = imageBuffersFromChannelArray(outputs, pointSize, nodeScaleFactor, this.layerRenderer2DPixelPositions);
+
+                for(let channel = 0; channel < numChannels; channel++) {
+
+                    if(showEllipse && channel === numChannels - 1) {
+                        channel = totalChannels - 1;
+
+                        let ellipseElem = nodeContainerElem.querySelector('.layer__ellipse-2d');
+                        if (!ellipseElem) {
+                            ellipseElem = document.createElement('div');
+                            ellipseElem.className = 'layer__ellipse-2d';
+                            ellipseElem.textContent = '...';
+                            Object.assign(ellipseElem.style, {
+                                font: '24px Arial',
+                                position: 'relative',
+                                top: '-8px',
+                                width: ellipseWidth + 'px'
+
+                            });
+                            nodeContainerElem.appendChild(ellipseElem);
+                        }
+
+                    }
+
                     // create off-screen canvas element
                     let canvas = getCanvasByName(nodeContainerElem, 'canvas-' + channel);
-                    canvas.style = 'border: 1px solid #838383';
+                    Object.assign(canvas.style, {
+                        border: '1px solid #838383',
+                        width: nodeWidth + 'px',
+                        height: nodeHeight + 'px'
+                    });
                     let ctx = canvas.getContext('2d');
                     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -115,8 +180,8 @@
                     .map(pixelPos => {
                         let canvasRect = this.layerRenderer2DNodeElems[pixelPos.channel].getBoundingClientRect();
                         return {
-                            x: canvasRect.left + pixelPos.x + document.documentElement.scrollLeft + pointSize/2,
-                            y: canvasRect.top + pixelPos.y + document.documentElement.scrollTop + pointSize/2
+                            x: canvasRect.left + pixelPos.x + document.documentElement.scrollLeft + pixelPos.pointSize/2,
+                            y: canvasRect.top + pixelPos.y + document.documentElement.scrollTop + pixelPos.pointSize/2
                         };
                     });
             }
